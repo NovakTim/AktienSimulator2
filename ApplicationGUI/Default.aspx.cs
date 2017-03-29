b2633222e9fba5defd6cde87bd087f9b99cfe39d
NovakTim <tim.novak@outlook.com>
2017-03-29 08:41:13 +0200

    Summary

--- ApplicationGUI/Default.aspx.cs
+++ ApplicationGUI/Default.aspx.cs
@@ -9,20 +9,21 @@ namespace AktienSimulator
         protected void btnLogin_Click(object sender, EventArgs e)
         {
             ErrorCodes.Login errorcode = ErrorCodes.Login.NoError;
+            // Versucht anzumelden
             var account = LogicAccount.LogIn(textNickname.Text, textPassword.Text, ref errorcode);
             switch (errorcode)
             {
-                case ErrorCodes.Login.NoError:
+                case ErrorCodes.Login.NoError: // Erfolgreich angemeldet
                     //Response.Write("<script>alert('Erfolgreich angemeldet.');</script>");
                     Account = account;
                     Response.Redirect("Boerse.aspx");
                     break;
 
-                case ErrorCodes.Login.NicknameNotFound:
+                case ErrorCodes.Login.NicknameNotFound: // Nickname wurde nicht gefunden
                     Response.Write("<script>alert('Es wurde kein Account unter dem angegeben Nicknamen gefunden.');</script>");
                     break;
 
-                case ErrorCodes.Login.WrongPassword:
+                case ErrorCodes.Login.WrongPassword: // Falsches Passwort eingegeben
                     Response.Write("<script>alert('Falsches Passwort.');</script>");
                     break;
 
@@ -33,14 +34,15 @@ namespace AktienSimulator
 
         protected void btnRegistrieren_Click(object sender, EventArgs e)
         {
+            //Versucht zu registrieren
             var errorcode = LogicAccount.RegisterAccount(textNickname.Text, textPassword.Text);
             switch (errorcode)
             {
-                case ErrorCodes.Register.NoError:
+                case ErrorCodes.Register.NoError: // Erfolgreich registriert
                     Response.Write("<script>alert('Erfolgreich registriert.');</script>");
                     break;
 
-                case ErrorCodes.Register.NameAlreadyTaken:
+                case ErrorCodes.Register.NameAlreadyTaken: // Nickname schon vergeben
                     Response.Write("<script>alert('Benutzername ist schon vergeben.');</script>");
                     break;
 
